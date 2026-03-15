fc095074304024717356d1283bc5747b7e825d63
wolfee <wolfezzz47@gmail.com>
2026-03-15 09:54:21 +0200

    add info in main project

---
+++ Project1_for_15.03.26/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<Windows.h>
+
+int main()
+{
+	using namespace std;
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+
+	cout << "привіт! гарний і чудовий ранок :)";
+
+	return 0;
+
+}
